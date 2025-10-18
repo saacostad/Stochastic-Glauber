ae990e6b8d890b2d5ff1d72810a63be9690a5b80
Santiago A <saacostad@unal.edu.co>
2025-10-18 13:38:50 -0500

    Fixed to accept multithreading random numbers generation

--- headers/geometryHeader.h
+++ headers/geometryHeader.h
@@ -3,14 +3,14 @@
 #include <gsl/gsl_sf_legendre.h>
 #include <random> 
 #include <cmath>
-// void test(std::string text){
-//     std::cout << text << std::endl;
-//     return;
-// }
+#include <vector> 
 
 
+#include "densityFunctions.h"
+
+// SPHERICAL HARMONICS 
 double Ylm(int l, int m, double theta, double phi) {
-    // Function that return the lm spherical harmonic evaluated in theta and phi 
+    // Function that return the real part of the lm spherical harmonic evaluated in theta and phi 
 
     double Plm = gsl_sf_legendre_sphPlm(l, std::abs(m), std::cos(theta)) * std::cos(m * phi);
 
@@ -21,6 +21,52 @@ double Ylm(int l, int m, double theta, double phi) {
 }
 
 
-//
-// double radialSample():
-//     // Returns a double which is a sample from the density distribution \rho(r)
+// ------------------------------
+// NUCLEON SAMPLING 
+// ------------------------------
+
+std::vector<double> angularDistribution(std::mt19937& gen){
+    // Returns two doubles: the position in theta and the position in phi sampled
+    // uniformly in a unitary sphere
+    using namespace std; 
+
+    uniform_real_distribution<double> dist_phi(0.0, 2.0 * M_PI);
+    uniform_real_distribution<double> dist_z(-1.0, 1.0); 
+
+    double phi = dist_phi(gen);
+    double theta = acos( dist_z(gen) );
+
+    vector ret = {theta, phi};
+
+    return ret;
+}   
+
+
+
+double radialSampling(double theta, double phi){
+    // This function samples a nucleon radially located in the angular coordinates theta and phi
+    // according to the radial density function 
+    
+    
+
+    return 0.0;
+}
+
+
+
+void sampleSphereSurface(int N, int l, int m){
+    // This function creates N^2 points over a unitary sphere and evaluates them on the 
+    // spherical harmonics lm
+    using namespace std;
+    for (double theta = 0; theta < M_PI; theta += (M_PI / double(N))){ 
+            for (double phi = -M_PI; phi < M_PI; phi += (M_PI / double(N))){
+                 
+                double x = sin(theta) * cos(phi);
+                double y = sin(theta) * sin(phi);
+                double z = cos(theta);
+
+               cout << x << "," << y << "," << z << "," << Ylm(l, m, theta, phi) << endl;            
+            }
+        }
+    return; 
+}
