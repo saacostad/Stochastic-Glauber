a85d97a08d69ba4a1d45345ece5b61b78b516145
Santiago A <saacostad@unal.edu.co>
2025-10-18 13:38:26 -0500

    Helps visualize the radial distribution

--- graph.py
+++ graph.py
@@ -3,25 +3,33 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 
-data = np.genfromtxt("data.csv", delimiter=",")
+data = np.genfromtxt("data.csv", delimiter="\n")
 
 print(data)
 
 
-x = data[:,0]
-y = data[:,1]
-z = data[:,2]
-c = data[:,3]
+counts, bin_edges = np.histogram(data, bins = 100)
+print(counts)
+print(bin_edges)
 
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-sc = ax.scatter(x, y, z, c=c, cmap='viridis')  # color by c
-plt.colorbar(sc, label='Color value')
-
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-
+plt.hist(data, bins=50, density=True, color='steelblue', alpha=0.7)
+plt.xlabel('Value')
+plt.ylabel('Probability Density')
 plt.show()
+# x = data[:,0]
+# y = data[:,1]
+# z = data[:,2]
+# c = data[:,3]
+#
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+#
+# sc = ax.scatter(x, y, z, c=c, cmap='viridis')  # color by c
+# plt.colorbar(sc, label='Color value')
+#
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# ax.set_zlabel('Z')
+#
+# plt.show()
