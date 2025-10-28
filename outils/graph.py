6106eb7f66ca7bceafd03a7715fdf391a6054d9b
Santiago A <saacostad@unal.edu.co>
2025-10-28 09:19:45 -0500

    For the presentation

--- outils/graph.py
+++ outils/graph.py
@@ -15,64 +15,71 @@ with open("systems/NNCrossSection.csv", newline='') as f:
 radius = np.sqrt(sigma_nn[200] / np.pi)
 
 
+# data1 = np.genfromtxt("systems/Au79-Au79/200/targets.csv", delimiter=",", max_rows=197)
+# data2 = np.genfromtxt("systems/Au79-Au79/200/projectiles.csv", delimiter=",", max_rows=197)
+
+# data1 = np.genfromtxt("systems/Eliptic/200/targets.csv", delimiter=",", max_rows=197)
+# data2 = np.genfromtxt("systems/Eliptic/200/projectiles.csv", delimiter=",", max_rows=197)
+
 data1 = np.genfromtxt("systems/GoldTest/200/targets.csv", delimiter=",", max_rows=197)
 data2 = np.genfromtxt("systems/GoldTest/200/projectiles.csv", delimiter=",", max_rows=197)
+
 
 x0 = data1[:197,0]
 y0 = data1[:197,1]
 z0 = data1[:197,2]
 
-x1 = data2[:197,0]
-y1 = data2[:197,1]
+x1 = data2[:197,0] 
+y1 = data2[:197,1] + 6.0
 z1 = data2[:197,2] + 20.0
 
 
 #
-# """ PYVISTA """
-# plotter = pv.Plotter()
-#
-# for (x, y, z), r in zip(np.array([x0, y0, z0]).T, [radius]*x0.size):
-#     sphere = pv.Sphere(radius=r, center=(x, y, z))
-#     plotter.add_mesh(sphere, color='blue', opacity=0.3)
-#
-# for (x, y, z), r in zip(np.array([x1, y1, z1]).T, [radius]*x0.size):
-#     sphere = pv.Sphere(radius=r, center=(x, y, z))
-#     plotter.add_mesh(sphere, color='red', opacity=0.3)
-#
-#
-#
-# # Add axes widget and coordinate frame
-# plotter.add_axes(interactive=True)
-# plotter.show_axes()
-#
-# # Add a bounding box for context
-# plotter.show_bounds(grid='front', location='outer', color='gray')
-# plotter.show()
+""" PYVISTA """
+plotter = pv.Plotter()
+
+for (x, y, z), r in zip(np.array([x0, y0, z0]).T, [radius]*x0.size):
+    sphere = pv.Sphere(radius=r, center=(x, y, z))
+    plotter.add_mesh(sphere, color='blue', opacity=0.3)
+
+for (x, y, z), r in zip(np.array([x1, y1, z1]).T, [radius]*x0.size):
+    sphere = pv.Sphere(radius=r, center=(x, y, z))
+    plotter.add_mesh(sphere, color='red', opacity=0.3)
 
 
 
-""" Matplotlib """
-fig, ax = plt.subplots()
+# Add axes widget and coordinate frame
+plotter.add_axes(interactive=True)
+plotter.show_axes()
 
-rad = 15
+# Add a bounding box for context
+plotter.show_bounds(grid='front', location='outer', color='gray')
+plotter.show()
 
-for (x, y), r in zip(np.array([z1, y1]).T, [radius]*x0.size):
-    circle = plt.Circle((x, y), r, fill=True, color='blue', lw=1.5,
-                        transform=ax.transData,
-                        alpha = 0.2)  # ✅ ensures "data units"
-    ax.add_patch(circle)
-for (x, y), r in zip(np.array([z0, y0]).T, [radius]*x0.size):
-    circle = plt.Circle((x, y), r, fill=True, color='red', lw=1.5,
-                        transform=ax.transData,
-                        alpha = 0.2)  # ✅ ensures "data units"
-    ax.add_patch(circle)
-# Equal aspect ratio so circles look like circles
-ax.set_aspect('equal')
 
-ax.set_xlim(-rad, rad + 20)
-ax.set_ylim(-rad, rad)
-plt.xlabel('z [fm]', fontsize = 18)
-plt.ylabel('y [fm]', fontsize = 18)
-plt.tick_params(axis='both', which='major', labelsize=16)  # ticks font size
-plt.grid(True, linestyle='--', alpha=0.5)
-plt.show()
+
+# """ Matplotlib """
+# fig, ax = plt.subplots()
+#
+# rad = 15
+#
+# for (x, y), r in zip(np.array([x1, y1]).T, [radius]*x0.size):
+#     circle = plt.Circle((x, y), r, fill=True, color='blue', lw=1.5,
+#                         transform=ax.transData,
+#                         alpha = 0.2)  # ✅ ensures "data units"
+#     ax.add_patch(circle)
+# for (x, y), r in zip(np.array([x0, y0]).T, [radius]*x0.size):
+#     circle = plt.Circle((x, y), r, fill=True, color='red', lw=1.5,
+#                         transform=ax.transData,
+#                         alpha = 0.2)  # ✅ ensures "data units"
+#     ax.add_patch(circle)
+# # Equal aspect ratio so circles look like circles
+# ax.set_aspect('equal')
+#
+# ax.set_xlim(-rad, rad + 3)
+# ax.set_ylim(-rad, rad + 6)
+# plt.xlabel('x [fm]', fontsize = 18)
+# plt.ylabel('y [fm]', fontsize = 18)
+# plt.tick_params(axis='both', which='major', labelsize=16)  # ticks font size
+# plt.grid(True, linestyle='--', alpha=0.5)
+# plt.show()
